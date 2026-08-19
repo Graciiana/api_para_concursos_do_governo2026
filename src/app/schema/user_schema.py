263df3ae103ff252a8d966fc6c934eb50079fe28
Graciana Tchivangulula <gtchivangulula@gmail.com>
2026-08-19 15:58:35 +0100

    fix: corrigir validacao dos campos de ActualizarUserSchema

--- src/app/schema/user_schema.py
+++ src/app/schema/user_schema.py
@@ -8,21 +8,21 @@ from app.models.models import RoleEnum
 class UserSchema(BaseModel):
     nome: str = Field(min_length=5, max_length=50)
     email: EmailStr
-    password: str = Field(max_length=8)
     criado_em: datetime
 
 
 class CriarUserSchema(UserSchema):
-    pass
+    password: str = Field(max_length=8)
+    role: RoleEnum = RoleEnum.USER
 
 
 class ActualizarUserSchema(BaseModel):
     nome: str | None = Field(default=None,min_length=10, max_length=50)
-    email: EmailStr | None 
-    password: str | None
+    email: EmailStr | None = Field(default=None)
+    password: str | None = Field(default=None, max_length=8)
     actualizado_em: datetime
 
 
 class UserSchemaResponse(UserSchema):
     id: int
-    role: RoleEnum = RoleEnum.USER 
+    role: RoleEnum
